e8a82d2824197662e54c24cd8a81edf153612d12
Yann Bertrand <yann@bertrand.com>
2015-06-30 21:04:19 +0200

    style: change the indention, end-of-line, etc...

--- index.js
+++ index.js
@@ -1,91 +1,104 @@
 #!/usr/bin/env node
 
-//sails-viewify modelname viewname config.js
+// sails-viewify modelname viewname config.js
 var fs = require('fs');
 var path = require('path');
 var str = '';
 jsStringEscape = require('js-string-escape');
 
-//init
-if(process.argv[2].localeCompare("init") === 0 ){
-	fs.exists('./.sailsrc', function(isSails){
-		if(!isSails)
-			throw new Error('not a sails project');
-		fs.createReadStream(path.join( __dirname , 'config.js'))
-			.pipe(fs.createWriteStream( path.join(process.cwd() , 'config/sails-viewify.js')));		
-	  	fs.writeFileSync('viewify_input.txt' , '');
-	});
+// Init
+if(process.argv[2].localeCompare("init") === 0) {
+  fs.exists('./.sailsrc', function(isSails){
+    if(!isSails)
+      throw new Error('Not a sails project');
+    fs.createReadStream(path.join( __dirname , 'config.js'))
+      .pipe(fs.createWriteStream( path.join(process.cwd() , 'config/sails-viewify.js')));   
+      fs.writeFileSync('viewify_input.txt' , '');
+  });
 }
-//escape
-else if(process.argv[2].localeCompare("escape") === 0 ){
-	fs.exists('./.sailsrc', function(isSails){
-		if(!isSails)
-			throw new Error('not a sails project');
-		fs.readFile('viewify_input.txt' , { encoding : 'utf8'} , function(err , data){
-			var mystr = jsStringEscape(data);
-			fs.writeFile('viewify_output.txt' , mystr , { encoding : 'utf8'} , function(err , data){
-				if(err)
-					throw err;
-			})
-		});
-	});
+
+// Escape
+else if(process.argv[2].localeCompare("escape") === 0) {
+  fs.exists('./.sailsrc', function(isSails) {
+    if(!isSails)
+      throw new Error('Not a sails project');
+    fs.readFile('viewify_input.txt', { encoding : 'utf8'}, function(err, data) {
+      var mystr = jsStringEscape(data);
+      fs.writeFile('viewify_output.txt', mystr, { encoding : 'utf8'}, function(err, data) {
+        if(err)
+          throw err;
+      });
+    });
+  });
 }
-else
-{
-	//other than init & escape 
-	//check if init is done 
-	fs.exists(path.join(process.cwd() , 'config/sails-viewify.js') , function(isInit){
-		if(!isInit)
-			new Error('sails-viewify not initialized.Try sails-viewify init');
-		else
-		{
-			if( process.argv[2] === undefined )
-				throw new Error('specify source filename');
 
-			if(process.argv[3] === undefined )
-				throw new Error('specify destination filename');
+else {
+  // Other than init & escape 
+  // Check if init is done 
+  fs.exists(path.join(process.cwd(), 'config/sails-viewify.js'), function(isInit) {
+    if(!isInit)
+      new Error('sails-viewify not initialized. Try sails-viewify init');
+    else {
+      if(process.argv[2] === undefined)
+        throw new Error('Specify source filename');
 
-			if(process.argv[4] === undefined)
-				var config = require(path.join(process.cwd() , 'config/sails-viewify.js'));
-			else
-				var config = require('./' + process.argv[4]);
+      if(process.argv[3] === undefined)
+        throw new Error('specify destination filename');
 
-			//dependent requires 
-			var model = require(path.join(process.cwd() , 'api' , 'models', process.argv[2] + '.js' ) );
-			var attributes = model.attributes ;
-			var template = config.template;
-			var destFile = path.join('views' , process.argv[3]);
+      var config;
+      if(process.argv[4] === undefined)
+        config = require(path.join(process.cwd() , 'config/sails-viewify.js'));
+      else
+        config = require('./' + process.argv[4]);
 
-			var render = function (htmltext , objName , obj,  template ) {
-				var specials = template.specials ;
-				for(var i = 0 ; i < specials.length ; i++ ){
-					var specialObj = specials[i];
-					if(specialObj.replacer.localeCompare("name") === 0 ){
-						//htmltext = htmltext.replace(specialObj.text, objName);
-						htmltext = htmltext.split(specialObj.text).join(objName);
-					}
-					else if ( obj[specialObj.replacer] !== undefined ){
-						//htmltext = htmltext.replace(specialObj.text,obj[specialObj.replacer]);
-						htmltext = htmltext.split(specialObj.text).join(obj[specialObj.replacer]);
-					}
-				}
-				return htmltext;
-			}
+      // Dependent requires 
+      var model = require(path.join(process.cwd(), 'api', 'models', process.argv[2] + '.js'));
+      var attributes = model.attributes ;
+      var template = config.template;
+      var destFile = path.join('views' , process.argv[3]);
 
-			for(var obj in attributes){
-				var type = attributes[obj].type ;	
-				for( var index in template ){
-					var t = template[index];
-					//if model has model or collection instead of type , then type === undefined , so 
-					if( type !== undefined ){
-						if(type.localeCompare(t.type) === 0 ){
-							var temp =  render(t.htmltext , obj , attributes[obj] , t )
-							str = str.concat(temp + '\n');
-							fs.writeFileSync( destFile , str );
-						}
-					}
-				}
-			}
-		}
-	});
+      var render = function (htmltext, objName, obj, template) {
+        var specials = template.specials;
+
+        for(var i = 0 ; i < specials.length ; i++){
+          var specialObj = specials[i];
+
+          if(specialObj.replacer.localeCompare("name") === 0) {
+            htmltext = htmltext.split(specialObj.text).join(objName);
+          }
+          else if(obj[specialObj.replacer] !== undefined) {
+            htmltext = 
+              htmltext
+                .split(specialObj.text)
+                .join(obj[specialObj.replacer]);
+          }
+        }
+
+        return htmltext;
+      };
+
+      var type;
+      var t;
+      var temp;
+      var obj;
+      var index;
+      for(obj in attributes) {
+        type = attributes[obj].type;
+
+        for(index in template) {
+          t = template[index];
+
+          // if model has model or collection instead of type,
+          // then type === undefined, so 
+          if(type !== undefined) {
+            if(type.localeCompare(t.type) === 0) {
+              temp = render(t.htmltext, obj, attributes[obj], t);
+              str = str.concat(temp + '\n');
+              fs.writeFileSync(destFile, str);
+            }
+          }
+        }
+      }
+    }
+  });
 }
